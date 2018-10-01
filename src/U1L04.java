0ebfe1cbf52dbbe6b67a34b8a8d184ce7e65a30a
Isabelle <hammeri@theellisschool.org>
2018-10-01 12:15:42 -0400

    completed!

--- src/U1L04.java
+++ src/U1L04.java
@@ -7,20 +7,46 @@ public class U1L04 {
 		public static void main(String[] args) {
 			
 			int computerNum = (int)(Math.random() * 100) + 1;
-			// should I put it like this? : String answer = JOptionPane.showInputDialog("Guess an integer between 1 and 100: ");
 			String input;
 			int userNum = -1;
-			do {
-				input = JOptionPane.showInputDialog(null, "Please enter a number in between 1 and 100.");
-				userNum = Integer.parseInt(input);
-			} while (userNum > 100 || userNum < 1);
+			int guesses = 0;
 			
+			// put all below in while loop
 			
+			while (computerNum != userNum) {
+				
+				do {
+					input = JOptionPane.showInputDialog(null, "Please enter a number in between 1 and 100.");
+					userNum = Integer.parseInt(input);
+				} while (userNum > 100 || userNum < 1);
+				guesses = guesses + 1;
+				
+				if (userNum > computerNum) {
+					System.out.println("Too high! Guess again.");
+					
+				}
+				
+				if (userNum < computerNum) {
+					System.out.println("Too low! Guess again.");
+				}
+		
+				
+				if (userNum == computerNum) {
+					System.out.println("That is correct! Your score is " + guesses + " guesses.");
+					
+				}
+				
+				if ((computerNum - userNum <= 10) && (computerNum - userNum >= -10)) {
+					System.out.println("Your guess is within 10 points of the answer.");
+				}
+				
+			}
+				
+			
+			//put all above in while loop
 			
 			
 			/* things I have to do:
-			 * - randomize a computer guess by using int computerNum = (int)(Math.random() * 100) + 1;
-			 * - allow user to guess in a loop which is exited by guessing correctly or inputting -1
 			 * - print a score at the end of how many guesses
 			 * - implement a loop to check the user's input so as not to allow input outside the
 			 *   1 to 100 range
